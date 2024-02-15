075cf3dd96a965abfa8a1959ee22a3b52f5affb6
Jimmy Nelson <nelson.jimmy@gmail.com>
2024-02-15 15:35:04 -0700

    Updates the listener to identify selected records and put them in the appropriate lookup field.
    Up Next: udpate record create method for new values that can't be found. dispatch toast message when record is created and clear field values.

--- force-app/main/default/lwc/recommendation/recommendation.js
+++ force-app/main/default/lwc/recommendation/recommendation.js
@@ -2,10 +2,42 @@ import { LightningElement } from 'lwc';
 
 
 export default class Recommendation extends LightningElement {
+    addedRecommendationType;
+    addedCreator;
+    addedRecommender;
+    addedGenre;
+    addedPlatform;
+
+    constructor() {
+        super();
+        this.template.addEventListener("recordselection", this.handleRecordSelection);
+      }
 
     handleRecordSelection(event) {
-        console.log('in the grandparent component');
-        console.log('event.target.value', event.target.value);
-        console.log('event.target.fieldName', event.target.fieldName);
+        try {
+            event.preventDefault();
+            console.log('in the grandparent component');
+            console.log('event', event);
+            let details = event.detail;
+            console.log('details', details)
+            console.log('details.fieldName', details.fieldName);
+            console.log('details.value', details.value);
+            if (details.fieldName == 'Recommended_By__c') {
+                console.log('recommender')
+                this.addedRecommender = details.value;
+            } else if (details.fieldName == 'Creator_Author__c') {
+                console.log('creator')
+                this.addedCreator = details.value;
+            } else if (details.fieldName == 'Recommendation_Type__c') {
+                this.addedRecommendationType = details.value;
+            } else if (details.fieldName == 'Genre__c') {
+                this.addedGenre = details.value;
+            } else if (details.fieldName == 'Platform__c') {
+                this.addedPlatform = details.value;
+            }
+
+        } catch (err) {
+            console.log('error', err);
+        }
     }
 }
